9f44619d3afd1481bf2f9184f08290cd3424429f
Arun Kumar Singh <arunsinghsnd@gmail.com>
2021-07-05 21:40:21 +0530

    added the result to the another section

---
+++ src/Results.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {" "}
+      {!pets.length ? (
+        <h2>No pets Found</h2>
+      ) : (
+        pets.map((pet) => (
+          <Pet
+            name={pet.name}
+            animal={pet.animal}
+            breed={pet.breed}
+            key={pet.id}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Results;

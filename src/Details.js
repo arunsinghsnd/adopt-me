b18c82861306f56ba19147b0f2354297edaa98bc
Arun Kumar Singh <arunsinghsnd@gmail.com>
2021-07-10 15:51:59 +0530

    Added the Deatils page

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 
-function Details() {
-  return (
-    <div>
-      <h1>Dtails page</h1>
-    </div>
-  );
+class Details extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const res = await fetch(
+      `https://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+
+    const json = await res.json();
+
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+          name: json.pets[0].name,
+        },
+        json.pets[0]
+      )
+    );
+  }
+
+  render() {
+    const { animal, breed, city, state, description, name } = this.state;
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Details;
+export default withRouter(Details);

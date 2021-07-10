98f1594fe47053da3ae136f48775e15c3f6bb8e4
Arun Kumar Singh <arunsinghsnd@gmail.com>
2021-07-10 18:37:01 +0530

    selecting thumb nail

--- src/Carousel.js
+++ src/Carousel.js
@@ -9,6 +9,12 @@ class Carousel extends Component {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
 
+  handleIndexClick = (event) => {
+    this.setState({
+      active: +event.target.dataset.index,
+    });
+  };
+
   render() {
     const { active } = this.state;
     const { images } = this.props;
@@ -23,6 +29,8 @@ class Carousel extends Component {
               alt={photo}
               className={index === active ? "active" : ""}
               alt="anmial thunbnail"
+              onClick={this.handleIndexClick}
+              data-index={index}
             />
           ))}
         </div>
